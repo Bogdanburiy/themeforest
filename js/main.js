7d01a1bc585bd6bc4abc8a75f9dc837923dbedca
Bogdan <bbogdan201265@gmail.com>
2022-08-20 10:12:09 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 "use strict";
 
 // 1.1 Sticky Header
-let header = document.querySelector('.header');
+// let header = document.querySelector('.header');
 
-if (header) {
-    function stickyHeader() {
-        if (window.pageYOffset > 10) {
-            header.classList.add('sticky-active');
-        } else {
-            header.classList.remove('sticky-active');
-        }
-    }
+// if (header) {
+//     function stickyHeader() {
+//         if (window.pageYOffset > 10) {
+//             header.classList.add('sticky-active');
+//         } else {
+//             header.classList.remove('sticky-active');
+//         }
+//     }
 
-    window.addEventListener('load', stickyHeader);
-    window.addEventListener('scroll', stickyHeader);
-}
+//     window.addEventListener('load', stickyHeader);
+//     window.addEventListener('scroll', stickyHeader);
+// }
 
 // 1.2 Anchor Prevent default
 let anchor = document.querySelectorAll('a[href="#"]');
@@ -42,10 +42,60 @@ document.addEventListener('click', function(e) {
 
 // 1.4 Accordion
 let accordion = document.getElementsByClassName('offer-collapse__item-head');
-let i;
 
-for (i = 0; accordion.length; i++) {
-    accordion[i].addEventListener('click', function() {
+for ( let item of accordion) {
+    item.addEventListener('click', function() {
         this.parentElement.classList.toggle('active');
     });
 }
+
+// 1.4 Change logo on hover block "Our clients"
+let imageGet = Array.from(document.querySelectorAll('.clients-list__item .strong'));
+let imageSet = document.querySelector('.clients-logo img');
+
+for ( let item of imageGet ) {
+    item.addEventListener('mouseover', function() {
+        let imageSrc = item.dataset.src;
+        imageSet.src = imageSrc;
+    });
+}
+
+// 1.5 Parallax srolling items
+// const scroller = new LocomotiveScroll({
+//     el: document.querySelector('[data-scroll-container]'),
+//     smooth: true
+// });
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// scroller.on('scroll', ScrollTrigger.update);
+
+// ScrollTrigger.scrollerProxy(
+//     '.page-container', {
+//         scrollTop(value) {
+//             return arguments.length ?
+//             scroller.scrollTo(value, 0, 0) :
+//             scroller.scroll.instance.scroll.y
+//         },
+//         getBoundingClientRect() {
+//             return {
+//                 left: 0, top: 0, 
+//                 width: window.innerWidth,
+//                 height: window.innerHeight
+//             }
+//         }
+//     }
+// );
+
+// ScrollTrigger.create({
+//     trigger: '.big-image',
+//     scroller: '.page-container',
+//     start: 'top+=5% 50%',
+//     end: 'bottom-=40% 50%',
+//     animation: gsap.to('.big-image', {backgroundSize: '120%'}),
+//     scrub: 2,
+// });
+
+// ScrollTrigger.addEventListener('refresh', () => scroller.update())
+
+// ScrollTrigger.refresh()
